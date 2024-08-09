210915e7b91318fe62c991356823f40d70f160cf
Franco B. <franco.barravecchia2002@gmail.com>
2024-08-09 13:10:50 -0300

    feat: HideOnClickOutside

---
+++ src/components/common/HideOnClickOutside.tsx
@@ -0,0 +1,39 @@
+import { ReactNode, useEffect, useRef } from "react"
+import { StyledComponent } from "styled-components"
+
+interface HideOnClickOutsideProps {
+    Wrapper: StyledComponent<"div", any>,
+    wrapperProps?: any
+    isOpen : boolean
+    onClose : () => void
+    children: ReactNode
+}
+
+const HideOnClickOutside = ({Wrapper, wrapperProps, isOpen, onClose, children} : HideOnClickOutsideProps) => {
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    const handleButtonClick = (event: MouseEvent) => {
+        if (containerRef.current && !containerRef.current.contains(event.target as Node)) {
+            console.log("calling close");
+            onClose()
+            return
+        }
+    }
+
+    useEffect(()=>{
+        if (isOpen)
+            document.addEventListener('mousedown', handleButtonClick)
+            return () => {
+                document.removeEventListener('mousedown', handleButtonClick)
+            }
+    }, [isOpen])
+    
+    if (!isOpen) return <></>
+    return (
+        <Wrapper ref={containerRef} {...wrapperProps}>
+            {children}
+        </Wrapper>
+    )
+}
+
+export default HideOnClickOutside
