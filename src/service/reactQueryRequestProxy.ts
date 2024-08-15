ce3ae657dcd2487245d79537c67d3af746ec7fff
Franco B. <franco.barravecchia2002@gmail.com>
2024-08-15 10:59:24 -0300

    feat: get and react to post in RQ

--- src/service/reactQueryRequestProxy.ts
+++ src/service/reactQueryRequestProxy.ts
@@ -1,13 +1,24 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import { useHttpRequestService } from "./HttpRequestService"
+import { staleTimeForPost } from "../util/Constants";
+import { ReactionRequest } from "./reactQueryInterfaces";
 
 
+//TODO: the data should not be passed here, but in the mutation when calling mutation, Should use the Options interface
 interface MutationOptions<T> {
-    data: T,
-    onSuccess?: ((data: any, variables: void, context: unknown) => Promise<unknown> | unknown) | undefined,
-    onError?: ((data: any, variables: void, context: unknown) => Promise<unknown> | unknown) | undefined,
+    readonly data: T,
+    readonly onSuccess?: ((data: any, variables: void, context: unknown) => Promise<unknown> | unknown) | undefined,
+    readonly onError?: ((data: any, variables: void, context: unknown) => Promise<unknown> | unknown) | undefined,
 }
 
+interface Options<TVariables> {
+    readonly onSuccess?: ((data: any, variables: TVariables, context: unknown) => Promise<unknown> | unknown) | undefined,
+    readonly onError?: ((data: any, variables: TVariables, context: unknown) => Promise<unknown> | unknown) | undefined,
+}
+
+
+//TODO: fix this, almost all hooks are being call more times than necessary? (I have seen in all places that mutations are used like this
+//so it might not be true, investigate farther)
 const useReactQueryProxy = () => {
     const httpService = useHttpRequestService()
     const queryClient = useQueryClient();
@@ -36,6 +47,32 @@ const useReactQueryProxy = () => {
             },
             onError: options.onError
         }),
+
+
+        useGetPostById: (postId: string) => useQuery({
+            queryKey: ['post', postId],
+            queryFn: () => httpService.getPostById(postId),
+            staleTime: staleTimeForPost
+        }),
+
+        useCreateReaction: (options: Options<ReactionRequest>) => useMutation({
+            mutationFn: (data : ReactionRequest) => httpService.createReaction(data.postId, data.reactionType),
+            onSuccess: (data: any, variables: ReactionRequest, context: unknown) => {
+                queryClient.invalidateQueries({queryKey: ['post', variables.postId]})
+                if (options.onSuccess) options.onSuccess(data, variables, context)
+            },
+            onError: options.onError
+        }),
+
+        useDeleteReaction: (options: Options<ReactionRequest>) => useMutation({
+            mutationFn: (data: ReactionRequest) => httpService.deleteReaction(data.postId, data.reactionType),
+            onSuccess: (data: any, variables: ReactionRequest, context: unknown) => {
+                queryClient.invalidateQueries({queryKey: ['post', variables.postId]})
+                if (options.onSuccess) options.onSuccess(data, variables, context)
+            },
+            onError: options.onError
+        })
+        
     }
 }
 
