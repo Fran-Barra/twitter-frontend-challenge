ca4d6871853712520542148254cc7b7fcf48128e
Franco B. <franco.barravecchia2002@gmail.com>
2024-08-20 11:20:37 -0300

    fix: moved provider to the right place

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import "./App.css";
 import { Layout } from "./components/layout/Layout";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 
-const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient} >
       <Layout />
-    </QueryClientProvider>
   );
 };
 
