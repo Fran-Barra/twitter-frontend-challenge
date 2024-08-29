db9b80fc3a57d5c5d1f6515733524a256ed28f42
Franco B. <franco.barravecchia2002@gmail.com>
2024-08-29 10:14:11 -0300

    fix: don't mix comments and feed

--- src/hooks/useGetComments.ts
+++ src/hooks/useGetComments.ts
@@ -2,17 +2,18 @@ import { useEffect, useState } from "react";
 import { useHttpRequestService } from "../service/HttpRequestService";
 import { setLength, updateFeed } from "../redux/user";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
+import { Post } from "../service";
 
 interface UseGetCommentsProps {
   postId: string;
 }
 
+
+//TODO: should paginate, should make a pagination method
 export const useGetComments = ({ postId }: UseGetCommentsProps) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
-  const posts = useAppSelector((state) => state.user.feed);
-
-  const dispatch = useAppDispatch();
+  const [posts, setPosts] = useState<Post[]>([]);
 
   const service = useHttpRequestService();
 
@@ -20,10 +21,10 @@ export const useGetComments = ({ postId }: UseGetCommentsProps) => {
     try {
       setLoading(true);
       setError(false);
-      service.getCommentsByPostId(postId).then((res) => {        
-        const updatedPosts = Array.from(new Set([...posts, ...res]))
-        dispatch(updateFeed(updatedPosts));
-        dispatch(setLength(updatedPosts.length));
+      service.getCommentsByPostId(postId).then((res) => {       
+        setPosts(oldPosts => {
+          return Array.from(new Set([...oldPosts, ...res]))
+        }) 
         setLoading(false);
       });
     } catch (e) {
